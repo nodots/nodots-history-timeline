d3aff349bdb8e6cdc7f097113495aefb061e9365
Ken Riley <kenr@nodots.com>
2024-08-03 11:59:35 -0600

    Adding AWS amplify packages

--- src/components/RtCard/index.tsx
+++ src/components/RtCard/index.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, CardHeader, Typography } from '@mui/material'
+import { Card, CardContent, CardHeader, Link, Typography } from '@mui/material'
 import { formatYear, RtEvent } from '../../types'
 
 interface Props {
@@ -47,6 +47,17 @@ export function RtCard({ rtEvent }: Props) {
         >
           {rtEvent.event}
         </Typography>
+        <Link
+          href="https://www.google.com/maps/place/Curia+di+Pompeo/@41.8953024,12.4739933,16.92z/data=!4m6!3m5!1s0x132f61879475f52d:0x6f50e8e3d8d4e9e1!8m2!3d41.895465!4d12.4765811!16s%2Fm%2F0n_gxtb?entry=ttu"
+          sx={{
+            color: '#bbb',
+            fontFamily: 'sans-serif',
+            fontSize: '0.8rem',
+            fontVariant: 'small-caps',
+          }}
+        >
+          map
+        </Link>
       </CardContent>
     </Card>
   )
