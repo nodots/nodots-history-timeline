d16a4166f31e056067f21961b795f3f43e45ce02
Ken Riley <kenr@nodots.com>
2024-08-04 13:05:08 -0600

    WIP: More layout changes

--- src/components/RtBody/index.tsx
+++ src/components/RtBody/index.tsx
@@ -10,6 +10,12 @@ export function RtBody({ rtEvent }: Props) {
     <Paper id="RtBody">
       <div id="RtEntrySidebar">
         <h1>{formatYear(rtEvent.year)}</h1>
+        <div className="rt-entry-sidebar-references">
+          <h3>Sources</h3>
+          <a href="https://www.livius.org/sources/content/appian/appian-caesars-funeral/">
+            Apian on Caesar's Funeral
+          </a>
+        </div>
       </div>
       <div id="RtEntryContent">
         <h2>{rtEvent.event}</h2>
